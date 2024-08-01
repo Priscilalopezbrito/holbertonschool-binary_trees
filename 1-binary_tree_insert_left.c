4391e91ea049cbaf7c68f342ac520ec806a00db5
Priscila Lopez <prisscilalb@gmail.com>
2024-08-01 13:44:56 -0400

    upload file 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,31 +9,23 @@
  **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
-	binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *left_node;
 
+	if (parent  == NULL)
+		return (NULL);
+	left_node = malloc(sizeof(binary_tree_t));
 	if (left_node == NULL)
 	{
 		return (NULL);
 	}
-
 	left_node->n = value;
 	left_node->parent = parent;
+	left_node->left = parent->left;
 	left_node->right = NULL;
-
-	if (parent->left == NULL)
+	if (parent->left != NULL)
 	{
-		left_node->left = NULL;
-	}
-	else
-	{
-		left_node->left = parent->left;
 		parent->left->parent = left_node;
 	}
 	parent->left = left_node;
 	return (left_node);
 }
-
