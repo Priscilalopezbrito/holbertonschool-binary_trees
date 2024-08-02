8792555644a2ec99ead36bb2007af86874215f4a
sigidibau <adriansantiago33@gail.com>
2024-08-02 10:48:21 -0400

    Fix function typo in the struct name

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,7 @@
  * Return: 1 if node is a leaf, 0 if it is not
  */
 
-int binary_tree_is_leaf(const binary_tree_T *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
