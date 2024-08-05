d064511c8dcdd9981d2d87be6901825d69b1d433
Priscila Lopez <prisscilalb@gmail.com>
2024-08-05 09:10:10 -0400

    Fixing parent pointer in file 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,17 +8,17 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
 	/*If node is NULL or the parent is NULL*/
 	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
-	if (parent->left == node)
+	binary_tree_t *parent = node->parent;
+
+		if (parent->left == node)
 	{
 		return (parent->right);
 	}
-
 	else if (parent->right == node)
 	{
 		return (parent->left);
